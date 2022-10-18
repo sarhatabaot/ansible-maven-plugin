1bcfc8b7e72027395726f43f09efdcdb922c65e2
Omer Oreg <sarhatabaot1@gmail.com>
2022-10-18 08:56:14 +0300

    Updated dependencies & plugins to latest versions

--- src/main/java/co/escapeideas/maven/ansible/AbstractAnsibleMojo.java
+++ src/main/java/co/escapeideas/maven/ansible/AbstractAnsibleMojo.java
@@ -19,7 +19,7 @@ import java.util.List;
 public abstract class AbstractAnsibleMojo extends AbstractMojo {
 
     private static final String NEW_LINE_SEPARATOR = System.getProperty("line.separator");
-    
+
     private static final String STDERR_LOG = "stderr.log";
     private static final String STDOUT_LOG = "stdout.log";
     private final int BUFFER_SIZE = getBufferSize();
